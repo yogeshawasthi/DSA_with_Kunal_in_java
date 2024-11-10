794f172b194698398b8e1e9360aa6bb128531c54
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2024-11-10 20:12:26 +0545

    few minor chages

--- src/yogesh/assignment/Palindrome.java
+++ src/yogesh/assignment/Palindrome.java
@@ -1,7 +1,15 @@
 package yogesh.assignment;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String str = in.nextLine();
+        System.out.println("You Entered the string: " + str);
+        System.out.print("Today i am unalbe to to do this program so let`s do it furter days" );
         
     }
 }
