df2b8495ac5265c7729ddabbb4a6b238e88db4a2
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-03-03 01:13:08 +0545

    few updates

---
+++ src/yogesh/lecture/fristjava_program/input.java
@@ -0,0 +1,10 @@
+package yogesh.lecture.fristjava_program;
+import java.util.Scanner;
+
+public class input {
+    public static void main(String[] args){
+        System.out.println("hi");
+        Scanner input = new Scanner(System.in);
+
+    }
+}
