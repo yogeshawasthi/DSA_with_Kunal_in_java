e8278bd19c5d184f4104897cd4ae854ee6701ad0
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-03-28 23:17:25 +0545

    rol

--- src/yogesh/lecture/fristjava_program/Inputs.java
+++ src/yogesh/lecture/fristjava_program/Inputs.java
@@ -9,7 +9,7 @@ public class Inputs {
          String name = input.nextLine();//input.nextline() will take only an whole line of string
          System.out.print("Enter Your Roll No: ");
         int rollno = input.nextInt();
-        System.out.println("Hi , " + name + " Your Roll no is " + rollno);
+        System.out.println("Hi , " + name + " Your Roll no is :  " + rollno);
 
         int a = 234_000_000;// underscore are ignored
         System.out.println(a);
