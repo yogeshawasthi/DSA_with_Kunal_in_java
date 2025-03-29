52d60b5ff8f7748789d0584411ecde022db8aff1
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-03-29 23:58:29 +0545

    few changes

--- src/yogesh/assignment/Conditionals/Leetcode.java
+++ src/yogesh/assignment/Conditionals/Leetcode.java
@@ -13,18 +13,17 @@ public class Leetcode {
 //        System.out.println("mod is working or not check "+ mod);
 
         int sum = 0;
-        long mul;
-        mul = 1;
+        long mul = 1;
         while (num>0) {
             sum = sum + num % 10;
             mul = mul * num % 10;
             num = num / 10;
         }
-        System.out.println(mul);
-        System.out.println("Multiplied = " + mul);
-        System.out.println("Sum of digits = " + sum);
-        long result = mul - sum;
-        System.out.println(" Result is : "+ result);
+//        System.out.println(mul);
+//        System.out.println("Multiplied = " + mul);
+//        System.out.println("Sum of digits = " + sum);
+//        long result = mul - sum;
+//        System.out.println(" Result is : "+ result);
 
     }
 
