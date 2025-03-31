7208683a7b93bf93b88660541c9ff0e90069e6ee
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-03-31 23:25:32 +0545

    Sum of two number

---
+++ src/yogesh/assignment/Conditionals/Sum.java
@@ -0,0 +1,12 @@
+package yogesh.assignment.Conditionals;
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args){
+        System.out.println("Enter two numbers: ");
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = a+b;
+        System.out.printf("Sum of two Number is : ", c);
+    }
+}
