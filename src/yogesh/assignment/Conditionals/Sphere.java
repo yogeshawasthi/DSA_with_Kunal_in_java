474eba5cae3052ee5a99a25e3d01f8ed05e44113
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-02 23:06:16 +0545

    Sphere
    area of sphere

---
+++ src/yogesh/assignment/Conditionals/Sphere.java
@@ -0,0 +1,12 @@
+package yogesh.assignment.Conditionals;
+import java.util.Scanner;
+
+public class Sphere {
+    public static void main(String[] args) {
+        System.out.println("Enter radius of sphere");
+        Scanner in = new Scanner(System.in);
+        int rad = in.nextInt();
+        int Area = (4/3)*(22/7)*rad*rad*rad;
+        System.out.println("Volume of Sphere : " +Area);
+    }
+}
