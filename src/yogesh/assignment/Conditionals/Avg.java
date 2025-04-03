5530b7bdd4e493519415775c53de1718b2d8796f
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-03 22:36:44 +0545

    average
    of n numbers

---
+++ src/yogesh/assignment/Conditionals/Avg.java
@@ -0,0 +1,19 @@
+package yogesh.assignment.Conditionals;
+import java.util.Scanner;
+
+public class Avg {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How many number you want to enter TO find avereage");
+        int num= input.nextInt();
+        int count=0;
+        for(int i=num;i>=1;i--){
+            System.out.println("Enter The numbers");
+            int in = input.nextInt();
+            count+=in;
+
+        }
+        float avg = (float) count/num;
+        System.out.println("Average is : "+avg);
+    }
+}
