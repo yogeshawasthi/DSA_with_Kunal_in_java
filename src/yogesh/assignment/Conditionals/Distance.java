8c641eccae7f7ca9f7db41f7230a61674053547a
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-04 23:13:50 +0545

    Update Distance.java
    minor changes

--- src/yogesh/assignment/Conditionals/Distance.java
+++ src/yogesh/assignment/Conditionals/Distance.java
@@ -6,10 +6,10 @@ import java.lang.Math;
 public class Distance {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.println("Enter the Cordinates of Frist Point");
+        System.out.println("Enter the Coordinates of Frist Point");
         int x1 = in.nextInt();
         int y1 = in.nextInt();
-        System.out.println("Enter the cordinates of Second Point");
+        System.out.println("Enter the coordinate of Second Point");
         int x2 = in.nextInt();
         int y2 = in.nextInt();
         double c = ((y2-y1)*(y2-y1)+(x2-x1)*(x2-x1));
