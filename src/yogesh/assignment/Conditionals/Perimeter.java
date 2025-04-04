92376845422804c1b4c6265a6c832b62ff04997c
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-04 22:48:22 +0545

    Perimeter
    of square

---
+++ src/yogesh/assignment/Conditionals/Perimeter.java
@@ -0,0 +1,12 @@
+package yogesh.assignment.Conditionals;
+import java.util.Scanner;
+
+public class Perimeter {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the Length of Square");
+        int sid = in.nextInt();
+        System.out.println("Perimeter of Square is :"+ sid*4);
+    }
+
+}
