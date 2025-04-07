8a7ab74cdaa9b50c7f9a5dcf74f0ee3cffa8f299
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-07 23:56:33 +0545

    cpga

--- src/yogesh/assignment/Conditionals/Gpa.java
+++ src/yogesh/assignment/Conditionals/Gpa.java
@@ -6,6 +6,16 @@ public class Gpa {
         Scanner in = new Scanner(System.in);
         System.out.println("How many subject you have : ");
         int num = in.nextInt();
-        
+        int count = 0;
+        while (num>=1){
+            System.out.println("Enter The Marks: ");
+            int mark = in.nextInt();
+            count+=mark;
+            num--;
+
+        }
+        float gpa =(float) (count/num)*4;
+        System.out.println("Gpa is : "+gpa);
+
     }
 }
