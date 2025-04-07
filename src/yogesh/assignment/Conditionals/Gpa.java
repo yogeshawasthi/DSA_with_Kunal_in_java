98fcc8bfba24df6e135d61ea42dca994ff6076cb
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-07 23:53:06 +0545

    gpa
    cgpa

---
+++ src/yogesh/assignment/Conditionals/Gpa.java
@@ -0,0 +1,11 @@
+package yogesh.assignment.Conditionals;
+import java.util.Scanner;
+
+public class Gpa {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("How many subject you have : ");
+        int num = in.nextInt();
+        
+    }
+}
