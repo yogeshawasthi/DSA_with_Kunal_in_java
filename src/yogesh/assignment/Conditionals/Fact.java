ca1d28233101cfdc71aa5cadd90cc4d57ac843a6
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-10 22:55:19 +0545

    factorial
    is update

--- src/yogesh/assignment/Conditionals/Fact.java
+++ src/yogesh/assignment/Conditionals/Fact.java
@@ -1,4 +1,5 @@
 package yogesh.assignment.Conditionals;
+import javax.xml.stream.events.EndDocument;
 import java.util.Scanner;
 
 public class Fact {
@@ -6,15 +7,18 @@ public class Fact {
         System.out.println("Enter a num for finding factorial Of : ");
         Scanner in = new Scanner(System.in);
         int num = in.nextInt();
-        int fact=1;
-        if(num==0){
-            System.out.println("Factorial of "+num+"is = 1");
-        }
-        while (num>=1){
-            fact*=num;
-            num--;
+        int fact = 1;
+        if (num == 0) {
+            System.out.println("Factorial of " + num + " is = 1");
 
         }
-        System.out.println("Factorial is :  "+fact);
+        if (num > 0) {
+            while (num >= 1) {
+                fact *= num;
+                num--;
+
+            }
+            System.out.println("Factorial is :  " + fact);
+        }
     }
 }
