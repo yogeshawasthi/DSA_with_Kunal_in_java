7010089fdc57bf9cd7d7b857d00d1713fe63f07a
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-10 22:47:43 +0545

    factroial
    with exception handling

---
+++ src/yogesh/assignment/Conditionals/Fact.java
@@ -0,0 +1,21 @@
+package yogesh.assignment.Conditionals;
+import java.util.Scanner;
+
+public class Fact {
+    public static void main(String[] args) {
+        System.out.println("Enter a num for finding factorial Of : ");
+        Scanner in = new Scanner(System.in);
+        int num = in.nextInt();
+        int fact=1;
+        if(num==0){
+            System.out.println("Factorial of "+num+"is = 1");
+        }
+        while (num>=1){
+            fact*=num;
+            num--;
+
+        }
+        System.out.println("Factorial is :  "+fact);
+    }
+}
+git
