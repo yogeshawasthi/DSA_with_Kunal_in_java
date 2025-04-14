d9552d67d4e2eacd43f277a8c066597d0908345c
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-14 22:30:10 +0545

    HCF

---
+++ src/yogesh/assignment/Conditionals/HCF.java
@@ -0,0 +1,19 @@
+package yogesh.assignment.Conditionals;
+import  java.util.Scanner;
+//Euclidean Algorithm
+
+public class HCF {
+    public static void main(String[] args) {
+        System.out.println("Enter Two number  For Finding HCF : ");
+        Scanner in = new Scanner(System.in);
+        int num1 = in.nextInt();
+        int num2 = in.nextInt();
+        int rem;
+
+        System.out.println("Let`s Find Out HCF: ");
+        if (num1>num2){
+            rem
+        }
+
+    }
+}
