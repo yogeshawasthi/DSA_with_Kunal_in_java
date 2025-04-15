9ada5be3e0914cc484f5099b0bdc7b96308498a8
Yogesh Awasthi <yogeshawasthi54321@gmail.com>
2025-04-15 23:37:56 +0545

    Update HCF.java
    s

--- src/yogesh/assignment/Conditionals/HCF.java
+++ src/yogesh/assignment/Conditionals/HCF.java
@@ -6,6 +6,7 @@ public class HCF {
     public static void main(String[] args) {
         System.out.println("Enter Two number  For Finding HCF : ");
         Scanner in = new Scanner(System.in);
+        
         int num1 = in.nextInt();
         int num2 = in.nextInt();
         int rem=num1;
